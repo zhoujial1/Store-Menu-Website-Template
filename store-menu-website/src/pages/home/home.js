de7ab0b9d0997df7e5b06f5a69bc64f1252ac469
zhoujial1 <zhoujial1@myvuw.ac.nz>
2024-07-18 23:16:18 +1200

    Implement contact and location section for the home page.

--- store-menu-website/src/pages/home/home.js
+++ store-menu-website/src/pages/home/home.js
@@ -16,10 +16,19 @@ function Home () {
 
       <div className='Section2'>
         <h1>Contacts</h1>
+        <div className='Contacts'>
+          <p>Phone: 123-456-7890</p>
+          <p>Email: Someemail@gmail.com</p>
+          <p>Fax: (123)-456-7890</p>
+        </div>
       </div>
 
       <div className='Section3'>
         <h1>Location</h1>
+        <div className='Location'>
+          <p>Address: 123 Main St, New York, NY 10001</p>
+          <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.171219648758!2d-73.98785308459425!3d40.74881797932853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259c90d7c4e5d%3A0x1f6b9c3d0b9c7b5d!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1632384094113!5m2!1sen!2sus' title='map' width='600' height='450' allowfullscreen='' loading='lazy'></iframe>
+        </div>
       </div>
     </div>
   );
