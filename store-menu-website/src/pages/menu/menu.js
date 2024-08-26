8af0ae4207eeef2f92e3cc3422b8b10c55109eb0
zhoujial1 <zhoujial1@myvuw.ac.nz>
2024-08-26 22:55:28 +1200

    Implement menu sections

--- store-menu-website/src/pages/menu/menu.js
+++ store-menu-website/src/pages/menu/menu.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import './menu.css';
 
 function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className='Background'>
+      <div className='Grid'>
+        <div className='Genre1'>
+          Deep Fried Food
+        </div>
+        <div className='Genre2'>
+          Burgers/Toast
+        </div>
+        <div className='Genre3'>
+          Chinese Food
+        </div>
+        <div className='Genre4'>
+          Packs
+        </div>
+      </div>
     </div>
   );
 }
