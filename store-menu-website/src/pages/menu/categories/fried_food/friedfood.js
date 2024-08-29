4b17cda55d491eab1ffbdb622019eec15f631121
zhoujial1 <zhoujial1@myvuw.ac.nz>
2024-08-30 01:55:53 +1200

    Implement fried food section

--- store-menu-website/src/pages/menu/categories/fried_food/friedfood.js
+++ store-menu-website/src/pages/menu/categories/fried_food/friedfood.js
@@ -3,7 +3,25 @@ import React from 'react';
 function Fried_food() {
     return (
         <div>
-            hello from Friedfood
+            <h1>Deep Fried</h1>
+            <div className='Grid'>
+                <div className='Item'>
+                    <img src='https://via.placeholder.com/300' alt='Deep Fried' />
+                    <div>Item 1</div>
+                </div>
+                <div className='Item'>
+                    <img src='https://via.placeholder.com/300' alt='Deep Fried' />
+                    <div>Item 2</div>
+                </div>
+                <div className='Item'>
+                    <img src='https://via.placeholder.com/300' alt='Deep Fried' />
+                    <div>Item 3</div>
+                </div>
+                <div className='Item'>
+                    <img src='https://via.placeholder.com/300' alt='Deep Fried' />
+                    <div>Item 4</div>
+                </div>
+            </div>
         </div>
     );
 }
